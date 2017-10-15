8eea9102fda545ceb87d8d165c51cd255b0104e9
rick <rick.yound@yahoo.com>
2017-10-15 16:32:21 +0800

    Tutorial:
    read image and show image in window.
    
    use cv2.imread()/cv2.imshow()/cv2.imwrite()

--- src/cpic.py
+++ src/cpic.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-import numpy as np
 import cv2
 
 img = cv2.imread('../sources/IMG_20171011_173400.jpg', 0)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
