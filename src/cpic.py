2c057d52e6b525cf3f4d0d42f36de4328ccd1518
rick <rick.yound@yahoo.com>
2017-10-16 10:21:57 +0800

    show the image in matplotlib

--- src/cpic.py
+++ src/cpic.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*- 
 
 import cv2
+from matplotlib import pyplot as plt
 
 img = cv2.imread('../sources/IMG_20171011_173400.jpg', 0)
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cv2.imwrite('messigray.png', img)
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.xticks([]), plt.yticks([]) # to hide the tick value on X and Y aixs
+plt.show()
 
