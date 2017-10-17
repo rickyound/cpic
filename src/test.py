85ab7347a476d796ab93948c164023b65a170a1d
rick <rick.yound@yahoo.com>
2017-10-17 09:53:40 +0800

    split channels smaple

--- src/test.py
+++ src/test.py
@@ -5,8 +5,11 @@ import cv2
 
 img = cv2.imread('../sources/IMG_20171011_173400.jpg', 1)
 
-flag = img[646:1282, 2466:3046]
-img[646:1282, 1466:2046] = flag
+b, g, r = cv2.split(img)
+img = cv2.merge((b, g, r))
+
+# make red pixels all to zero
+img[:,:,2] = 0
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.imshow('image', img)
