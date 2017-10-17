af700b407d9100376ad666c80683c76d1e33b010
rick <rick.yound@yahoo.com>
2017-10-17 17:05:42 +0800

    test Image Blending

--- src/test.py
+++ src/test.py
@@ -7,13 +7,10 @@ import numpy as np
 img01 = cv2.imread('../sources/opencv-logo.jpg', 1)
 img02 = cv2.imread('../sources/ewm.jpg', 1)
 
-timg01 = cv2.add(img01, img02)
-timg02 = img01 + img02
+dst = cv2.addWeighted(img01, 0.7, img02, 0.3, 0)
 
-cv2.namedWindow('image1', cv2.WINDOW_NORMAL)
-cv2.namedWindow('image2', cv2.WINDOW_NORMAL)
-cv2.imshow('image1', timg01)
-cv2.imshow('image2', timg02)
+cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
+cv2.imshow('image', dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
