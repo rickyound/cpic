75c627d5e023b36d1e5d09c4a890ab75f2528c5f
rick <rick.yound@yahoo.com>
2017-10-18 17:02:35 +0800

    adjust sources file's size

--- src/cpic.py
+++ src/cpic.py
@@ -2,12 +2,21 @@
 # -*- coding: utf-8 -*- 
 
 import cv2
-from matplotlib import pyplot as plt
 
-img = cv2.imread('../sources/IMG_20171011_173400.jpg', 0)
+img = cv2.imread('../sources/img_small.jpg')
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.imshow('images', img)
+ret, thresh = cv2.threshold(imgray, 127, 255, 0) 
+
+cv2.namedWindow('original', cv2.WINDOW_NORMAL)
+cv2.namedWindow('final', cv2.WINDOW_NORMAL)
+
+cv2.imshow('original', img)
+cv2.imshow('final', thresh)
+
+#image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+#cv2.imshow('images', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
