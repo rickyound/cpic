20abedd4c4a57b6f2f3e7f40b8189e11c2d8641e
rick <rick.yound@yahoo.com>
2017-10-19 16:51:38 +0800

    test adaptive thresholding

--- src/temp/test_adaptive_thresholding.py
+++ src/temp/test_adaptive_thresholding.py
@@ -8,9 +8,19 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img_gray = cv2.imread('../../sources/sudoku.jpg', 0)
+#img_gray = cv2.medianBlur(img_gray, 3)
 
-ret, thresh1 = cv2.threshold(img_gray, 110, 255, cv2.THRESH_BINARY)
+ret, thresh1 = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY)
 
-cv2.imshow('thresh1', thresh1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+thresh2 = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 2)
+thresh3 = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 2)
+
+titles = ['Original Image', 'Simple Thresholding(v=100)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img_gray, thresh1, thresh2, thresh3]
+
+for i in xrange(4):
+    plt.subplot(2,2,(i+1)), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
