1fcb319338685d7183debf64ad804327aae5c5a2
rick <rick.yound@yahoo.com>
2017-10-19 14:48:27 +0800

    test for simple thresholding

---
+++ src/temp/test_simple_thresholding.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 演示各个类型的的阈值化表现
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img_gray = cv2.imread('../../sources/gradient.jpg', 0)
+
+ret, thresh1 = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img_gray, 127, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img_gray, 127, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img_gray, 127, 255, cv2.THRESH_TOZERO_INV)
+
+titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img_gray, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+for i in xrange(6):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
