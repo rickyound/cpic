695a8c448d692a779a16b5b543ab7f1a08405374
rick <rick.yound@yahoo.com>
2017-10-26 10:46:17 +0800

    modified some numbers

--- src/cpic.py
+++ src/cpic.py
@@ -10,14 +10,18 @@ img_ori = cv2.imread('../sources/img_little.jpg')
 img = cv2.cvtColor(img_ori, cv2.COLOR_BGR2RGB)
 
 rows, cols, ch = img.shape
+# rows=388, cols=520, ch=3
 
 # 重点在于这几个点如何获取
-pts1 = np.float32([[69, 30], [473, 30], [4, 354], [500, 354]])
-pts2 = np.float32([[0, 0], [496, 0], [0, 324], [496, 324]])
+pts1 = np.float32([[72, 10], [483, 10], [0, rows], [cols, rows]])
+pts2 = np.float32([[0, 0], [cols, 0], [0, rows], [cols, rows]])
 
 M = cv2.getPerspectiveTransform(pts1, pts2)
 
-dst = cv2.warpPerspective(img, M, (496, 324))
+dst = cv2.warpPerspective(img, M, (cols, rows))
+
+cv2.imwrite('../targets/img_little.jpg', img_ori)
+cv2.imwrite('../targets/img_little_dst.jpg', cv2.cvtColor(dst, cv2.COLOR_RGB2BGR))
 
 plt.subplot(121), plt.imshow(img), plt.title('Input'), plt.xticks([]), plt.yticks([])
 plt.subplot(122), plt.imshow(dst), plt.title('Output'), plt.xticks([]), plt.yticks([])
